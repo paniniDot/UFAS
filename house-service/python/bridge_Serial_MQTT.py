f81882f8dfbd4d3ae08b7c3a18d2fc03b70c2549
riccardo fiorani <riccardofiorani@hotmail.it>
2024-04-23 23:11:45 +0200

    add mqtt in python

--- house-service/python/bridge_Serial_MQTT.py
+++ house-service/python/bridge_Serial_MQTT.py
@@ -1,7 +1,9 @@
 ### author: Roberto Vezzani
 import configparser
-
 import paho.mqtt.client as mqtt
+from websockets.server import serve
+from websockets.sync.client import connect
+import asyncio
 
 class Bridge():
 
@@ -10,7 +12,20 @@ class Bridge():
 		self.config.read('config.ini')
 
 		self.pubtopic = self.config.get("MQTT","PubTopic", fallback= "room1/cam")
+		self.setupWS()
 		self.setupMQTT()
+		
+
+	def setupWS(self):
+		self.serverWS = serve(self.ws_handler, "localhost", 8080)
+		self.clientWS = connect("ws://192.168.1.51:8080")
+		print("Websocket server started on localhost:8080")
+	
+	async def ws_handler(self, websocket, path):
+		print("Websocket connection established")
+		data = await websocket.recv()
+		print(f"Received message from WS: {data}")
+
 
 	def setupMQTT(self):
 		self.clientMQTT = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -18,7 +33,7 @@ class Bridge():
 		self.clientMQTT.on_message = self.on_message
 		print("Connecting to MQTT broker...")
 		self.clientMQTT.connect(
-			self.config.get("MQTT","Server", fallback= "192.168.1.66"),
+			self.config.get("MQTT","Server", fallback= "192.168.1.51"),
 			self.config.getint("MQTT","Port", fallback= 1883),
 			60)
 
@@ -37,11 +52,14 @@ class Bridge():
 	# The callback for when a PUBLISH message is received from the server.
 	def on_message(self, client, userdata, msg):
 		print("Received message from MQTT")
-		print(msg.topic + " " + str(msg.payload))
+		#print(msg.topic + " " + str(msg.payload))
+		self.clientWS.send(str(msg.payload))
+		
 
 
 if __name__ == '__main__':
 	br=Bridge()
 	br.clientMQTT.loop_forever()
+	
 	
 
