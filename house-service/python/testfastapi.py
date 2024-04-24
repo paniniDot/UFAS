db3b7c3be9fd90901aec7eb91ab6bc231d4dd6c5
riccardo fiorani <riccardofiorani@hotmail.it>
2024-04-24 13:31:42 +0200

    add mqtt in fastapi

--- house-service/python/testfastapi.py
+++ house-service/python/testfastapi.py
@@ -1,8 +1,15 @@
 # FastAPI application
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
-
+from fastapi_mqtt.fastmqtt import FastMQTT
+from fastapi_mqtt.config import MQTTConfig
 app = FastAPI()
+
+mqtt_config = MQTTConfig()
+
+fast_mqtt = FastMQTT(config=mqtt_config)
+
+fast_mqtt.init_app(app)
 
 html = """
 <!DOCTYPE html>
@@ -49,4 +56,33 @@ async def websocket_endpoint(websocket: WebSocket):
         data = await websocket.receive_text()
         await websocket.send_text(f"Message text was: {data}")
 
+
+@fast_mqtt.on_connect()
+def connect(client, flags, rc, properties):
+    client.subscribe("/mqtt") #subscribing mqtt topic
+    print("Connected: ", client, flags, rc, properties)
+
+@fast_mqtt.on_message()
+async def message(client, topic, payload, qos, properties):
+    print("Received message: ",topic, payload.decode(), qos, properties)
+    return 0
+
+@fast_mqtt.subscribe("my/mqtt/topic/#")
+async def message_to_topic(client, topic, payload, qos, properties):
+    print("Received message to specific topic: ", topic, payload.decode(), qos, properties)
+
+@fast_mqtt.on_disconnect()
+def disconnect(client, packet, exc=None):
+    print("Disconnected")
+
+@fast_mqtt.on_subscribe()
+def subscribe(client, mid, qos, properties):
+    print("subscribed", client, mid, qos, properties)
+
+
+@app.get("/")
+async def func():
+    fast_mqtt.publish("/mqtt", "Hello from Fastapi") #publishing mqtt topic
+
+    return {"result": True,"message":"Published" }
 
