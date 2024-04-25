e9981dfd8b2aa3585422105dfc0b4c809380c381
Mattia Panni <pnmattia@gmail.com>
2024-04-25 10:07:06 +0200

    FastAPI implementation and mosquitto server conf

--- house-service/python/testfastapi.py
+++ house-service/python/testfastapi.py
@@ -1,52 +1,77 @@
-# FastAPI application
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi_mqtt.fastmqtt import FastMQTT
 from fastapi_mqtt.config import MQTTConfig
+import asyncio
+import uvicorn
+
 app = FastAPI()
 
-mqtt_config = MQTTConfig()
+mqtt_config = MQTTConfig(host="localhost", port=1883)
 
 fast_mqtt = FastMQTT(config=mqtt_config)
 
 fast_mqtt.init_app(app)
 
+messages = []
+
+@fast_mqtt.on_connect()
+def connect(client, flags, rc, properties):
+    print("Connected: ", client, flags, rc, properties)
+
+@fast_mqtt.on_disconnect()
+def disconnect(client, packet, exc=None):
+    print("Disconnected")
+
+@fast_mqtt.subscribe("room1/cam")
+async def cam_handler(client, topic, payload, qos, properties):
+    print("Received message on topic: ", topic)
+    messages.append(payload.decode())
+
 html = """
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
+<head> 
+    <title>WebSocket Test</title>
+</head>
+<body>
+    <h1>WebSocket Webcam</h1>
+    
+    <img id="image" src="" alt="Live Webcamp">
+    
+    <script>
+        document.addEventListener("DOMContentLoaded", function(event) {
+            var ws = new WebSocket("ws://localhost:8080/ws");
+            ws.onopen = function(event) {
+                console.log("WebSocket connection established.");
             };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
+            ws.onmessage = function(event) {
+                console.log("Message received:", event.data);
+                // Parse message JSON
+                var message = JSON.parse(event.data);
+                // Get the base64 encoded image data
+                var imageData = message.measure;
+                // Update the image src with the new data
+                document.getElementById("image").src = imageData;
+            };
+            ws.onerror = function(event) {
+                console.error("WebSocket error:", event);
+            };
+            ws.onclose = function(event) {
+                console.log("WebSocket connection closed.");
+            };
+            setInterval(function() {
+                ws.send("Hello, server!");
+            }, 1000);
+        });
+    </script>
+</body>
 </html>
+
 """
 
 @app.get("/")
-async def get():
+async def root():
     return HTMLResponse(html)
 
 @app.websocket("/ws")
@@ -54,35 +79,13 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        print(f"Message received: {data}")
+        if messages:
+            message = messages.pop(0)
+            await websocket.send_text(message)
+        else:
+            await asyncio.sleep(0.01)
 
 
-@fast_mqtt.on_connect()
-def connect(client, flags, rc, properties):
-    client.subscribe("/mqtt") #subscribing mqtt topic
-    print("Connected: ", client, flags, rc, properties)
-
-@fast_mqtt.on_message()
-async def message(client, topic, payload, qos, properties):
-    print("Received message: ",topic, payload.decode(), qos, properties)
-    return 0
-
-@fast_mqtt.subscribe("my/mqtt/topic/#")
-async def message_to_topic(client, topic, payload, qos, properties):
-    print("Received message to specific topic: ", topic, payload.decode(), qos, properties)
-
-@fast_mqtt.on_disconnect()
-def disconnect(client, packet, exc=None):
-    print("Disconnected")
-
-@fast_mqtt.on_subscribe()
-def subscribe(client, mid, qos, properties):
-    print("subscribed", client, mid, qos, properties)
-
-
-@app.get("/")
-async def func():
-    fast_mqtt.publish("/mqtt", "Hello from Fastapi") #publishing mqtt topic
-
-    return {"result": True,"message":"Published" }
-
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8080)
